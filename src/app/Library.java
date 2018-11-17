bb13e7f68dcd4baa15c3ff587a8dfb174cf63660
alezmu <aleksandra.m.zmudzinska@gmail.com>
2018-11-17 17:40:18 +0100

    Add dataReader

--- src/app/Library.java
+++ src/app/Library.java
@@ -1,5 +1,6 @@
 package app;
 import data.Book;
+import utils.DataReader;
 
 class Library{
 	public static void main (String[] args){
@@ -7,18 +8,23 @@ class Library{
 		final String appName ="Biblioteka v0.5";
 
 		Book [] books = new Book[1000];
-		//
-		books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890" );
-		books[1] = new Book("Java. Efektywne programowanie. Wydanie II", " Joshua Bloch", 2009, 352, "Helion", "9788324620845" );
-		books[2] = new Book("Muza", "Jessie Burton", 2016, 480, "Wydawnictwo Literackie", "9788308062357" );
-		books[3]= new Book("Swing Time", "Zadie Smith", 2017, 480, "Znak", "9788324050130" );
-		
+
+		DataReader dataReader = new DataReader();
 		System.out.println(appName);
+		System.out.println("Wprowadz nowa ksiazke: ");
+
+		//
+		books[0] = dataReader.readAndCreateBook();
+		books[1] = dataReader.readAndCreateBook();
+		//books[2] = new Book("Muza", "Jessie Burton", 2016, 480, "Wydawnictwo Literackie", "9788308062357" );
+		//books[3]= new Book("Swing Time", "Zadie Smith", 2017, 480, "Znak", "9788324050130" );
+		dataReader.close();
+
 		System.out.println("Ksiazki dostepne w bibliotece: ");
 		books[0].printInfo();
 		books[1].printInfo();
-		books[2].printInfo();
-		books[3].printInfo();
+		System.out.println("Biblioteka moze przechowywac do "+books.length+" ksiazek");
+
         	
 	
 	}	
---
+++ src/utils/DataReader.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.util.Scanner;
+import data.Book;
+
+public class DataReader {
+
+    private Scanner sc;
+
+    public DataReader(){
+        sc = new Scanner(System.in);
+    }
+
+    public void close(){
+        sc.close();
+    }
+
+    public Book readAndCreateBook(){
+        System.out.println("Tytul: ");
+        String title =sc.nextLine();
+        System.out.println("Autor: ");
+        String author = sc.nextLine();
+        System.out.println("Wydawnictowo: ");
+        String publisher = sc.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = sc.nextLine();
+        System.out.println("Rok wydania: ");
+        int releseDate = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ilosc stron: ");
+        int pages = sc.nextInt();
+        sc.nextLine();
+
+        return new Book(title, author, releseDate, pages, publisher, isbn);
+    }
+
+}
